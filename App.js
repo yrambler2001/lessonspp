ea78062070bdea5a909213fba37670e636db6569
Yuriy Synyshyn <yrambler2001@gmail.com>
2019-09-02 01:02:34 +0300

    some table parsing code

--- App.js
+++ App.js
@@ -26,29 +26,26 @@ const fetchJSON = url =>
     request.onload = () => {
       if (request.status === 200) {
         resolve(iconv.decode(Buffer.from(request.response), 'windows-1251'));
-      } else {
-        reject(new Error(request.statusText));
-      }
+      } else { reject(new Error(request.statusText)); }
     };
     request.onerror = () => reject(new Error(request.statusText));
     request.responseType = 'arraybuffer';
 
     request.open('GET', url);
-    request.setRequestHeader(
-      'Content-type',
-      'text/plain; charset=windows-1251',
-    );
+    request.setRequestHeader('Content-type', 'text/plain; charset=windows-1251');
     request.send();
   });
 
-function findNodeDFS(document, id) {
+function findNodeDFS(document, { tag, name, value, isMulti }, accumulator = [],
+) {
   for (const object of Array.isArray(document) ? document : [document]) {
-    if (object && object.attrs && _.find(object.attrs, { value: id })) {
-      return object;
-    }
-    const fromRecursive =
-      object && object.childNodes && findNodeDFS(object.childNodes, id);
-    if (fromRecursive) {
+    if (object &&
+      (tag ? object.tagName === tag || object.tag === tag || object.nodeName === tag : true) &&
+      (name && value ? object.attrs && _.find(object.attrs, { value, name }) : true)
+    ) { if (isMulti) { accumulator.push(object); } else { return object; } }
+    const fromRecursive = object && object.childNodes &&
+      findNodeDFS(object.childNodes, { tag, name, value, isMulti }, accumulator);
+    if (fromRecursive && !isMulti) {
       return fromRecursive;
     }
   }
@@ -67,10 +64,36 @@ const App = () => {
     f();
   }, []);
 
-  const table = useMemo(
-    () => document && findNodeDFS(document, 'ScheduleWeek'),
-    [document],
-  );
+  const isFirstWeek = useMemo(() => {
+    if (document) {
+      const h3 = findNodeDFS(document, { tag: 'h3', name: 'class', value: 'Black', });
+      const isFirstWeek = _.find(h3.childNodes, child => _.includes(child.value, 'перший'));
+      const isSecondWeek = _.find(h3.childNodes, child => _.includes(child.value, 'другий'));
+      if (!(isFirstWeek || isSecondWeek)) { alert('invalid week'); }
+      return !!isFirstWeek;
+    }
+  }, [document]);
+
+  const table = useMemo(() => document && findNodeDFS(document, { tag: 'table', name: 'id', value: 'ScheduleWeek', }), [document]);
+
+  const willRenameLater = useMemo(() => {
+    if (table) {
+      const linesInTable = []; findNodeDFS(table, { tag: 'tr', isMulti: true }, linesInTable);
+
+      const iHeader = []
+      const q = 0;
+      let k = 0;
+      const headr = []; findNodeDFS(linesInTable[0], { tag: 'th', isMulti: true }, headr);
+      headr.forEach((header, index) => {
+        const countOfGroupsInRow = (_.find(header.attrs, { name: 'colspan', value: '2' })) ? 2:1
+        iHeader[index] = countOfGroupsInRow;
+        k += 1;
+        iHeader[index] = countOfGroupsInRow;
+        k += 1;
+      })
+      debugger;
+    }
+  }, [table]);
 
   console.log(table);
 
